df09fc6d4047396eef228707587f834e2fd8f118
Adam Ochonicki <adam@fromonesrc.com>
2013-05-21 21:39:04 -0400

    Let's spec

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,13 @@
-class pycharm (
-  $version = '2.7.1',
-) {
+# Public: Installs PyCharm
+#
+# Usage:
+#
+#   include pycharm
+class pycharm {
+  $version = '2.7.1'
+
   package { 'PyCharm':
     provider => 'appdmg',
-    source   => "http://download.jetbrains.com/python/pycharm-${version}.dmg",
+    source   => "http://download.jetbrains.com/python/pycharm-${version}.dmg"
   }
 }
---
+++ spec/classes/pycharm_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'pycharm' do
+  it do
+    should contain_package('PyCharm').with({
+      :source   => "http://download.jetbrains.com/python/pycharm-2.7.1.dmg",
+      :provider => 'appdmg'
+    })
+  end
+end
