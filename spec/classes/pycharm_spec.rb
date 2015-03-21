4e00eab90ef51c2fd310e2ec2d79ed46ebb39f91
Ryan Fink <ryanjfink@gmail.com>
2015-03-21 09:03:17 -0400

    Fix endpoint for pycharm

--- spec/classes/pycharm_spec.rb
+++ spec/classes/pycharm_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'pycharm' do
   it do
     should contain_package('PyCharm').with({
-      :source   => "http://download.jetbrains.com/python/pycharm-professional-3.0.dmg",
+      :source   => "http://download.jetbrains.com/python/pycharm-community-4.0.5.dmg",
       :provider => 'appdmg'
     })
   end
